f1afce3bcf2889531212dc0fcb848c5a82da6529
Dwij Bavisi <dwij@dbavisi.net>
2024-10-09 09:30:40 +0000

    Create Bucket and Configure Identities; Release 0; HotFix 1

---
+++ 0.0.1/src/infrastructure/setup.py
@@ -0,0 +1,29 @@
+# Amaterasu Particle
+# Dwij Bavisi <dwij@dbavisi.net>
+
+"""
+Release 0, Hotfix Pack 0, Hotfix 1
+
+This module serves as the entry point for setting up the infrastructure for the project.
+"""
+
+import os
+import dotenv
+import boto3
+
+from s3.mailbox import create_mailbox
+from ses.identities import configure_identities
+
+if __name__ == '__main__':
+    dotenv.load_dotenv()
+
+    access_key = os.getenv('AWS_access_key')
+    secret_access_key = os.getenv('AWS_secret_access_key')
+    boto3.setup_default_session(aws_access_key_id=access_key, aws_secret_access_key=secret_access_key)
+
+    region = os.getenv('AWS_region', 'us-east-1')
+    environment = os.getenv('AWS_environment', 'delta')
+    rootDomain = os.getenv('AWS_rootDomain', 'dbavisi.net')
+
+    bucket = create_mailbox(region, environment, rootDomain)
+    domain = configure_identities(region, environment, rootDomain)
